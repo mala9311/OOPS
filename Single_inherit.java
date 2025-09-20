731ccdc5668258402a2c80cd68b49367ebbd772a
mala9311 <malakumari9311@gmail.com>
2025-09-20 13:50:39 +0530

    single_inheritance

---
+++ Single_inherit.java
@@ -0,0 +1,35 @@
+class one{
+    String Name;
+    int Age;
+    void output(){
+        System.out.println("Name is :" + Name);
+        System.out.println("Age is :" + Age);
+    }
+}
+class two extends one{
+
+}
+
+public class Single_inherit{
+    public static void main(String[] args){
+        two T1 = new two();
+        T1.Name = "Mala";
+        T1.Age = 21;
+        T1.output();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+ 
+    
+
+
+
